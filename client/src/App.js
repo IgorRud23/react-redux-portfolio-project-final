ab82d76f42ab05bb1d48f3eeee3b1137d09ab426
IgorRud23 <rud.igor23@gmail.com>
2019-12-08 16:45:56 -0500

    init header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-    <h1>Books</h1>
-    
+    <Header/>
+    <Footer/>
     </div>
   );
 }
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+    <h6>© {new Date().getFullYear()}, All rights reserved.</h6>
+    </footer>
+  )
+};
+
+export default Footer;
