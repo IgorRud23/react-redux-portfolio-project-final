c57d8224f046cca8d56abdb15d44980bdda8bcf8
IgorRud23 <rud.igor23@gmail.com>
2020-01-23 20:58:10 -0500

    small changes

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -7,7 +7,7 @@ const About = () => {
     {`
       We helping people to exchange knowledge and explore the world of books.
       With us, will be easier to pick the right book for yourself and share books that inspired you.
-      Feel free to add your favorite books on our list, more books - more knowledge.
+      Feel free to add your favorite books on our list, more books - more knowledge. Join us today.
     `}
     </Typography>
   )
